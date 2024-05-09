4a0ebbe996bddeac70f08f23046dfebf71108a0d
root <sthabile151@gmail.com>
2024-05-10 01:38:20 +0200

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,45 @@
 #!/usr/bin/python3
+""" Queries Reddit API for number of subscribers in given subreddit
+    Returns 0 if invalid subreddit
 """
-this module contains a function that queries the Reddit API and returns the
-number of subscribers for a given subreddit, if an invalid subreddit is
-given, the function should return 0
-"""
-
-import requests
+import requests as r
+import requests.auth as ra
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """
-    queries the Reddit API and returns number of subscribers for a given
-    subreddit
-    """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    h = {'User-agent': 'ad-egg'}
-    r = requests.get(url, headers=h, allow_redirects=False)
 
-    if r.status_code == 200:
-        req = r.json()
-        info = req.get('data')
-        subscribers = info.get('subscribers')
-        if info is not None and subscribers is not None:
-            return subscribers
-    return 0
+    # Generates token & bearer for requests to oauth.reddit.com endpoints
+    # https://github.com/reddit-archive/reddit/wiki/OAuth2-Quick-Start-Example
+
+    client_id = "i_m3aNLQE0vskA"
+    secret = "UykV3Qz2TGSWSWaMmSeOWgR5JT4"
+    my_username = "causticpop"
+    my_pwd = "Holberton98"
+
+    client_auth = ra.HTTPBasicAuth(client_id, secret)
+    post_data = {"grant_type": "password", "username":
+                 my_username, "password": my_pwd}
+    headers = {"User-Agent": "ChangeMeClient/0.1 by {}".format(my_username)}
+    response = r.post("https://www.reddit.com/api/v1/access_token",
+                      auth=client_auth, data=post_data, headers=headers)
+    token = response.json().get("access_token")
+    bearer = response.json().get("token_type")
+
+    # Queries oauth.reddit.com endpoints using token
+
+    subreddit = argv[1]
+    sub_url = "https://oauth.reddit.com/r/{}/about".format(subreddit)
+
+    headers = {"Authorization": "{} {}".format(bearer, token),
+               "User-Agent": "ChangeMeClient/0.1 by {}".format(my_username)}
+    response = r.get(sub_url, headers=headers)
+
+    # handles error response; invalid subreddit
+    if response.status_code is not 200:
+        return(0)
+    else:
+        response_json = response.json().get('data')
+        result = response_json.get('subscribers')
+
+        return result
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,33 +1,47 @@
 #!/usr/bin/python3
+""" Queries Reddit API for first 10 hot post titles from subreddit
+    Prints 'None' if invalid subreddit
 """
-this module contains a function that queries the Reddit API and prints the
-titles of the first 10 hot posts listed for a given subreddit
-"""
-
-import requests
+import requests as r
+import requests.auth as ra
+from sys import argv
 
 
 def top_ten(subreddit):
-    """
-    function queries the Reddit API and prints the titles of the first 10 hot
-    posts listed for a given subreddit
-    """
 
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    h = {'User-agent': 'ad-egg'}
-    r = requests.get(
-            url, headers=h, allow_redirects=False, params={'limit': 10})
+    # Generates token & bearer for requests to oauth.reddit.com endpoints
+    # https://github.com/reddit-archive/reddit/wiki/OAuth2-Quick-Start-Example
 
-    if r.status_code == 200:
-        req = r.json()
-        data = req.get('data')
-        children = data.get('children')
-        if data is None or children is None:
-            print('None')
-        else:
-            for post in children:
-                post_data = post.get('data')
-                title = post_data.get('title')
-                print(title)
+    client_id = "i_m3aNLQE0vskA"
+    secret = "UykV3Qz2TGSWSWaMmSeOWgR5JT4"
+    my_username = "causticpop"
+    my_pwd = "Holberton98"
+
+    client_auth = ra.HTTPBasicAuth(client_id, secret)
+    post_data = {"grant_type": "password", "username":
+                 my_username, "password": my_pwd}
+    headers = {"User-Agent": "ChangeMeClient/0.1 by {}".format(my_username)}
+    response = r.post("https://www.reddit.com/api/v1/access_token",
+                      auth=client_auth, data=post_data, headers=headers)
+    token = response.json().get("access_token")
+    bearer = response.json().get("token_type")
+
+    # Queries oauth.reddit.com endpoints using token & bearer
+    # Sets limit on number of items returned in params variable
+
+    subreddit = argv[1]
+    sub_url = "https://oauth.reddit.com/r/{}/hot".format(subreddit)
+
+    headers = {"Authorization": "{} {}".format(bearer, token),
+               "User-Agent": "ChangeMeClient/0.1 by {}".format(my_username)}
+    params = {'limit': 10}
+    response = r.get(sub_url, headers=headers, params=params)
+
+    # handles error response; invalid subreddit
+    if response.status_code is not 200:
+        print("None")
+    # peels the onion of nested dicts and lists
     else:
-        print('None')
+        response_json = response.json().get('data').get('children')
+        for subdict in response_json:
+            print(subdict.get('data').get('title'))
