a43338029677416c6d094b17256e748d87796e85
SisandaSH <ntombifuthishwala1978@gmail.com>
2024-05-16 03:07:09 -0700

    web_stack_debugging_4

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,8 @@
+exec { 'fix-os-config':
+  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command => "sed -ir 's/^holberton hard nofile 5$/holberton hard nofile 50000/g' /etc/security/limits.conf",
+}
+exec { 'fix-os-config2':
+  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command => "sed -ir 's/^holberton soft nofile 4$/holberton hard nofile 40000/g' /etc/security/limits.conf",
+}
